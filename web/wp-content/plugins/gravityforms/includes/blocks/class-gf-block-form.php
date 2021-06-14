dbdc833c89a6d1fa7795cf3f9fb225a42950f7d1
Neil Arnold <neilparnold@gmail.com>
2021-06-14 13:42:24 -0400

    update lock

--- web/wp-content/plugins/gravityforms/includes/blocks/class-gf-block-form.php
+++ web/wp-content/plugins/gravityforms/includes/blocks/class-gf-block-form.php
@@ -43,7 +43,9 @@ class GF_Block_Form extends GF_Block {
 		'description' => array( 'type' => 'boolean' ),
 		'ajax'        => array( 'type' => 'boolean' ),
 		'tabindex'    => array( 'type' => 'string' ),
+		'fieldValues' => array( 'type' => 'string' ),
 		'formPreview' => array( 'type' => 'boolean' ),
+		'imgPreview'  => array( 'type' => 'boolean' ),
 	);
 
 	/**
@@ -62,20 +64,6 @@ class GF_Block_Form extends GF_Block {
 		return self::$_instance;
 
 	}
-
-	/**
-	 * Register needed hooks.
-	 *
-	 * @since 2.4.10
-	 */
-	public function init() {
-
-		parent::init();
-
-		add_action( 'wp_enqueue_scripts', array( $this, 'maybe_enqueue_form_scripts' ) );
-
-	}
-
 
 
 
@@ -118,7 +106,9 @@ class GF_Block_Form extends GF_Block {
 			$script['handle'],
 			'gform_block_form',
 			array(
-				'forms' => $this->get_forms(),
+				'adminURL' => admin_url( 'admin.php' ),
+				'forms'    => $this->get_forms(),
+				'preview'  => GFCommon::get_base_url() . '/images/gf_block_preview.svg',
 			)
 		);
 
@@ -142,7 +132,20 @@ class GF_Block_Form extends GF_Block {
 
 		// Add Gravity Forms styling if CSS is enabled.
 		if ( '1' !== get_option( 'rg_gforms_disable_css', false ) ) {
-			$deps = array_merge( $deps, array( 'gforms_formsmain_css', 'gforms_ready_class_css', 'gforms_browsers_css' ) );
+			$deps = array_merge( $deps, array( 'gform_basic', 'gforms_formsmain_css', 'gforms_ready_class_css', 'gforms_browsers_css', 'gform_theme' ) );
+
+			/**
+			 * Allows users to disable the main theme.css file from being loaded on the Front End.
+			 *
+			 * @since 2.5-beta-3
+			 *
+			 * @param boolean Whether to disable the theme css.
+			 */
+			$disable_theme_css = apply_filters( 'gform_disable_form_theme_css', false );
+
+			if ( ! $disable_theme_css ) {
+				$deps[] = 'gform_theme';
+			}
 		}
 
 		return array(
@@ -155,104 +158,6 @@ class GF_Block_Form extends GF_Block {
 		);
 
 	}
-
-	/**
-	 * Parse current post's blocks for Gravity Forms block and enqueue required form scripts.
-	 *
-	 * @since  2.4.10
-	 */
-	public function maybe_enqueue_form_scripts() {
-
-		global $wp_query;
-
-		if ( ! isset( $wp_query->posts ) || ! is_array( $wp_query->posts ) ) {
-			return;
-		}
-
-		foreach ( $wp_query->posts as $post ) {
-
-			if ( ! $post instanceof WP_Post ) {
-				continue;
-			}
-
-			if ( ! has_block( $this->type, $post->post_content ) ) {
-				continue;
-			}
-
-			$blocks = parse_blocks( $post->post_content );
-
-			// Get form IDs for blocks.
-			$form_ids = $this->get_block_form_ids( $blocks );
-
-			// If no form IDs were found, skip.
-			if ( empty( $form_ids ) ) {
-				continue;
-			}
-
-			// Load GFFormDisplay.
-			if ( ! class_exists( 'GFFormDisplay' ) ) {
-				require_once( GFCommon::get_base_path() . '/form_display.php' );
-			}
-
-			// Enqueue scripts for found forms.
-			foreach ( $form_ids as $form_id => $ajax ) {
-				$form = GFAPI::get_form( $form_id );
-				GFFormDisplay::enqueue_form_scripts( $form, $ajax );
-			}
-
-		}
-
-	}
-
-	/**
-	 * Check current blocks and inner blocks for Gravity Forms block and return their form IDs.
-	 *
-	 * @since 2.4.11
-	 *
-	 * @param array $blocks Array of blocks.
-	 *
-	 * @return array
-	 */
-	private function get_block_form_ids( $blocks ) {
-
-		$form_ids = array();
-
-		foreach ( $blocks as $block ) {
-
-			// If block has inner blocks, add to form IDs array.
-			if ( rgar( $block, 'innerBlocks' ) ) {
-
-				// Get nested form IDs.
-				$nested_form_ids = $this->get_block_form_ids( $block['innerBlocks'] );
-
-				// Merge arrays.
-				if ( ! empty( $nested_form_ids ) ) {
-					$form_ids = array_replace( $form_ids, $nested_form_ids );
-				}
-
-			}
-
-			// If this is not a Form block or the form ID is not defined, skip.
-			if ( $this->type !== rgar( $block, 'blockName' ) || ( $this->type === rgar( $block, 'blockName' ) && ! rgars( $block, 'attrs/formId' ) ) ) {
-				continue;
-			}
-
-			// Get the form ID and AJAX attributes.
-			$form_id = (int) $block['attrs']['formId'];
-			$ajax    = rgars( $block, 'attrs/ajax' ) ? (bool) $block['attrs']['ajax'] : false;
-
-			// Add form ID to return array.
-			if ( ! in_array( $form_id, $form_ids ) || ( in_array( $form_id, $form_ids ) && true === $ajax && false === $form_ids[ $form_id ] ) ) {
-				$form_ids[ $form_id ] = $ajax;
-			}
-
-		}
-
-		return $form_ids;
-
-	}
-
-
 
 
 	// # BLOCK RENDER -------------------------------------------------------------------------------------------------
@@ -269,11 +174,12 @@ class GF_Block_Form extends GF_Block {
 	public function render_block( $attributes = array() ) {
 
 		// Prepare variables.
-		$form_id     = rgar( $attributes, 'formId' ) ? $attributes['formId'] : false;
-		$title       = isset( $attributes['title'] ) ? $attributes['title'] : true;
-		$description = isset( $attributes['description'] ) ? $attributes['description'] : true;
-		$ajax        = isset( $attributes['ajax'] ) ? $attributes['ajax'] : false;
-		$tabindex    = isset( $attributes['tabindex'] ) ? intval( $attributes['tabindex'] ) : 0;
+		$form_id      = rgar( $attributes, 'formId' ) ? $attributes['formId'] : false;
+		$title        = isset( $attributes['title'] ) ? $attributes['title'] : true;
+		$description  = isset( $attributes['description'] ) ? $attributes['description'] : true;
+		$ajax         = isset( $attributes['ajax'] ) ? $attributes['ajax'] : false;
+		$tabindex     = isset( $attributes['tabindex'] ) ? intval( $attributes['tabindex'] ) : 0;
+		$field_values = isset( $attributes['fieldValues'] ) ? $attributes['fieldValues'] : null;
 
 		// If form ID was not provided or form does not exist, return.
 		if ( ! $form_id || ( $form_id && ! GFAPI::get_form( $form_id ) ) ) {
@@ -286,8 +192,15 @@ class GF_Block_Form extends GF_Block {
 			// Start output buffering.
 			ob_start();
 
+			// Prepare field values.
+			if ( ! empty( $field_values ) ) {
+				$field_values = str_replace( '&#038;', '&', $field_values );
+				parse_str( $field_values, $field_value_array );
+				$field_values = stripslashes_deep( $field_value_array );
+			}
+
 			// Get form output string.
-			$form_string = gravity_form( $form_id, $title, $description, false, null, $ajax, $tabindex, false );
+			$form_string = gravity_form( $form_id, $title, $description, false, $field_values, $ajax, $tabindex, false );
 
 			// Get output buffer contents.
 			$buffer_contents = ob_get_contents();
@@ -298,7 +211,17 @@ class GF_Block_Form extends GF_Block {
 
 		}
 
-		return sprintf( '[gravityforms id="%d" title="%s" description="%s" ajax="%s" tabindex="%d"]', $form_id, ( $title ? 'true' : 'false' ), ( $description ? 'true' : 'false' ), ( $ajax ? 'true' : 'false' ), $tabindex );
+		// Encode field values.
+		$field_values = htmlspecialchars( $field_values );
+		$field_values = str_replace( array( '[', ']' ), array( '&#91;', '&#93;' ), $field_values );
+
+		// If no field values are set, set field values to null.
+		parse_str( $field_values, $field_value_array );
+		if ( empty( $field_value_array ) ) {
+			$field_values = null;
+		}
+
+		return sprintf( '[gravityforms id="%d" title="%s" description="%s" ajax="%s" tabindex="%d" field_values="%s"]', $form_id, ( $title ? 'true' : 'false' ), ( $description ? 'true' : 'false' ), ( $ajax ? 'true' : 'false' ), $tabindex, $field_values );
 
 	}
 
@@ -326,7 +249,7 @@ class GF_Block_Form extends GF_Block {
 		}
 
 		// Get form objects.
-		$form_objects = GFAPI::get_forms();
+		$form_objects = GFAPI::get_forms( true, false, 'title', 'ASC' );
 
 		// Loop through forms, add conditional logic check.
 		foreach ( $form_objects as $form ) {
@@ -337,7 +260,14 @@ class GF_Block_Form extends GF_Block {
 			);
 		}
 
-		return $forms;
+		/**
+		 * Modify the list of available forms displayed in the Form block.
+		 *
+		 * @since 2.4.23
+		 *
+		 * @param array $forms A collection of active forms on site.
+		 */
+		return apply_filters( 'gform_block_form_forms', $forms );
 
 	}
 
